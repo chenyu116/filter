ed38004ff8c1e8b80e73d817b545e96aa1567e63
Roger <cheny.roger@gmail.com>
2019-04-02 14:44:29 +0800

    fix escape

--- filter.go
+++ filter.go
@@ -6,83 +6,81 @@ import (
 )
 
 type filterRule struct {
-	Name     string
-	Value    string
-	Type     filterType
-	Required bool
-	Default  interface{}
+	Name          string
+	In            string
+	OutString     string
+	OutInt        int
+	Type          filterType
+	Required      bool
+	DefaultString string
+	DefaultInt    int
 }
 
 type filterType string
 
 type filter struct {
-	rules  []filterRule
-	data   map[string]interface{}
-	errMsg string
+	rules []filterRule
 }
 
-func (f *filter) AddRule(rule ...filterRule) {
+func (f filter) AddRule(rule ...filterRule) {
 	f.rules = append(f.rules, rule...)
 }
 
-func (f *filter) Map() map[string]interface{} {
-	return f.data
-}
-
-func (f *filter) Get(key string) interface{} {
-	if data, ok := f.data[key]; ok {
-		return data
-	}
-	return nil
-}
-
-func (f *filter) ErrorMsg() string {
-	return f.errMsg
-}
-
-func (f *filter) Valid() bool {
+func (f filter) GetString(key string) string {
 	for _, v := range f.rules {
-		v.Value = strings.Trim(v.Value, " ")
-		if v.Value == "" {
-			f.data[v.Name] = v.Default
-			if v.Required && v.Default == nil {
-				f.errMsg = v.Name + " is empty"
+		if v.Name == key {
+			return v.OutString
+		}
+	}
+	return ""
+}
+
+func (f filter) GetInt(key string) int {
+	for _, v := range f.rules {
+		if v.Name == key {
+			return v.OutInt
+		}
+	}
+	return -1
+}
+
+func (f filter) Valid() bool {
+	var r int
+	var err error
+	for _, v := range f.rules {
+		v.In = strings.Trim(v.In, " ")
+		if v.In == "" {
+			if v.Required && v.DefaultString == "nil" {
 				return false
 			}
+			v.OutString = v.DefaultString
 			continue
 		}
 		if v.Type == FilterTypeUint() {
-			r, err := strconv.Atoi(v.Value)
+			r, err = strconv.Atoi(v.In)
 			if err != nil {
-				if v.Default != nil {
-					r = v.Default.(int)
-				} else if v.Required {
-					f.errMsg = v.Name + " is invalid"
+				if v.Required && v.DefaultInt == -1 {
 					return false
 				} else {
-					f.data[v.Name] = nil
+					v.OutInt = v.DefaultInt
 					continue
 				}
 			}
 			if r < 0 {
-				f.errMsg = v.Name + " is invalid"
 				return false
 			}
-			f.data[v.Name] = r
+			v.OutInt = r
 		} else if v.Type == FilterTypeString() {
-			f.data[v.Name] = v.Value
+			v.OutString = v.In
 		} else {
-			f.errMsg = "Unknow type " + string(v.Type)
 			return false
 		}
 	}
 	return true
 }
 
-func NewFilter() *filter {
-	return &filter{
-		data: make(map[string]interface{}),
-	}
+func NewFilter() filter {
+	return filter{}
 }
 
 func FilterTypeUint() filterType {
@@ -93,32 +91,22 @@ func FilterTypeString() filterType {
 	return "string"
 }
 
-func Rule(name, value string, ft filterType, required bool, df interface{}) filterRule {
+func RuleUint(name, value string, required bool, df int) filterRule {
 	return filterRule{
-		Name:     name,
-		Value:    value,
-		Type:     ft,
-		Default:  df,
-		Required: required,
+		Name:       name,
+		In:         value,
+		Type:       FilterTypeUint(),
+		DefaultInt: df,
+		Required:   required,
 	}
 }
 
-func RuleUint(name, value string, required bool, df interface{}) filterRule {
+func RuleString(name, value string, required bool, df string) filterRule {
 	return filterRule{
-		Name:     name,
-		Value:    value,
-		Type:     FilterTypeUint(),
-		Default:  df,
-		Required: required,
-	}
-}
-
-func RuleString(name, value string, required bool, df interface{}) filterRule {
-	return filterRule{
-		Name:     name,
-		Value:    value,
-		Type:     FilterTypeString(),
-		Default:  df,
-		Required: required,
+		Name:          name,
+		In:            value,
+		Type:          FilterTypeString(),
+		DefaultString: df,
+		Required:      required,
 	}
 }
