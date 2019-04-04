fb47598bb5987f4861706794881c5d93f31cc04e
Roger <cheny.roger@gmail.com>
2019-04-04 17:34:32 +0800

    add func

--- filter.go
+++ filter.go
@@ -1,15 +1,16 @@
 package filter
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type filterRule struct {
 	Name          string
-	In            string
-	OutString     string
-	OutInt        int
+	Value         string
 	Type          filterType
 	Required      bool
 	DefaultString string
@@ -18,69 +19,91 @@ type filterRule struct {
 
 type filterType string
 
-type filter struct {
-	rules []filterRule
-}
-
-func (f filter) AddRule(rule ...filterRule) {
-	f.rules = append(f.rules, rule...)
-}
-
-func (f filter) GetString(key string) string {
-	for _, v := range f.rules {
+func GetString(rules []filterRule, key string) string {
+	for _, v := range rules {
 		if v.Name == key {
-			return v.OutString
+			if v.Value == "" {
+				return v.DefaultString
+			}
+			return v.Value
 		}
 	}
 	return ""
 }
 
-func (f filter) GetInt(key string) int {
-	for _, v := range f.rules {
+func GetInt(rules []filterRule, key string) int {
+	for _, v := range rules {
 		if v.Name == key {
-			return v.OutInt
+			v.Value = strings.Trim(v.Value, " ")
+			if v.Value == "" {
+				return v.DefaultInt
+			}
+			r, err := strconv.Atoi(v.Value)
+			if err != nil || r < 0 {
+				return v.DefaultInt
+			}
+			return r
 		}
 	}
 	return -1
 }
 
-func (f filter) Valid() bool {
+func Valid(rules ...filterRule) ([]filterRule, bool) {
 	var r int
 	var err error
-	for _, v := range f.rules {
-		v.In = strings.Trim(v.In, " ")
-		if v.In == "" {
-			if v.Required && v.DefaultString == "nil" {
-				return false
+	for _, v := range rules {
+		v.Value = strings.Trim(v.Value, " ")
+		if v.Value == "" {
+			if v.Required && v.DefaultString == "" {
+				return nil, false
 			}
-			v.OutString = v.DefaultString
 			continue
 		}
 		if v.Type == FilterTypeUint() {
-			r, err = strconv.Atoi(v.In)
+			r, err = strconv.Atoi(v.Value)
 			if err != nil {
 				if v.Required && v.DefaultInt == -1 {
-					return false
+					return nil, false
 				} else {
-					v.OutInt = v.DefaultInt
 					continue
 				}
 			}
 			if r < 0 {
-				return false
+				return nil, false
 			}
-			v.OutInt = r
+			continue
 		} else if v.Type == FilterTypeString() {
-			v.OutString = v.In
+			continue
 		} else {
-			return false
+			return nil, false
 		}
 	}
-	return true
+	return rules, true
 }
 
-func NewFilter() filter {
-	return filter{}
+func ValidToken(r []filterRule, KEY, token string) bool {
+	var keys []string
+	for k := range r {
+		if r[k].Required {
+			keys = append(keys, r[k].Name)
+		}
+	}
+	sort.Strings(keys)
+	var tokenString string
+	for k := range keys {
+		for rk := range r {
+			if r[rk].Name == keys[k] {
+				tokenString += r[rk].Value
+				break
+			}
+		}
+	}
+	tokenBytes := md5.Sum([]byte(tokenString + KEY))
+	_token := hex.EncodeToString(tokenBytes[:])
+	if strings.Compare(token, _token) != 0 {
+		return false
+	}
+	return true
 }
 
 func FilterTypeUint() filterType {
@@ -94,7 +117,7 @@ func FilterTypeString() filterType {
 func RuleUint(name, value string, required bool, df int) filterRule {
 	return filterRule{
 		Name:       name,
-		In:         value,
+		Value:      value,
 		Type:       FilterTypeUint(),
 		DefaultInt: df,
 		Required:   required,
@@ -104,7 +127,7 @@ func RuleUint(name, value string, required bool, df int) filterRule {
 func RuleString(name, value string, required bool, df string) filterRule {
 	return filterRule{
 		Name:          name,
-		In:            value,
+		Value:         value,
 		Type:          FilterTypeString(),
 		DefaultString: df,
 		Required:      required,
